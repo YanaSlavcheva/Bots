c564c5e84b2f664a234caf0e8eab3527f1d2556b
YanaSlavcheva <yana.slavcheva@gmail.com>
2017-09-23 18:55:49 +0300

    added var instead of int

--- ZaraCodeWeek/ZaraCodeWeek/Dialogs/RootDialog.cs
+++ ZaraCodeWeek/ZaraCodeWeek/Dialogs/RootDialog.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Connector;
-
-namespace ZaraCodeWeek.Dialogs
+﻿namespace ZaraCodeWeek.Dialogs
 {
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+
     [Serializable]
     public class RootDialog : IDialog<object>
     {
@@ -20,7 +20,7 @@ namespace ZaraCodeWeek.Dialogs
             var activity = await result as Activity;
 
             // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            var length = (activity.Text ?? string.Empty).Length;
 
             // return our reply to the user
             await context.PostAsync($"You sent {activity.Text} which was {length} characters");
