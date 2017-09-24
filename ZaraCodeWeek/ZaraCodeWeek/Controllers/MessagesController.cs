8d98418b9621051fb046cc8183ebabf9e3f1c4de
YanaSlavcheva <yana.slavcheva@gmail.com>
2017-09-24 13:05:28 +0300

    Made bot ask for name and repeat what user says

--- ZaraCodeWeek/ZaraCodeWeek/Controllers/MessagesController.cs
+++ ZaraCodeWeek/ZaraCodeWeek/Controllers/MessagesController.cs
@@ -1,39 +1,92 @@
-﻿using System;
-
-namespace ZaraCodeWeek.Controllers
+﻿namespace ZaraCodeWeek.Controllers
 {
+    using System;
     using System.Net;
     using System.Net.Http;
+    using System.Text;
     using System.Threading.Tasks;
     using System.Web.Http;
-    using Microsoft.Bot.Builder.Dialogs;
     using Microsoft.Bot.Connector;
 
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+        private const string KeyAskedForUsernameString = "AskedForUsername";
+        private const string KeyUsernameString = "Username";
+
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
+            //Global values
+            var haveAskedForUsername = false;
+            var username = string.Empty;
+
             if (activity.Type == ActivityTypes.Message)
             {
+                // Get any saved values
+                var stateClient = activity.GetStateClient();
+                var userData = stateClient.BotState.GetPrivateConversationData(
+                    activity.ChannelId,
+                    activity.Conversation.Id,
+                    activity.From.Id);
+                haveAskedForUsername = userData.GetProperty<bool>(KeyAskedForUsernameString);
+                username = userData.GetProperty<string>(KeyUsernameString) ?? string.Empty;
+
+                // Create text for reply message
+                var replyMessageStringBuilder = new StringBuilder();
+                if (!haveAskedForUsername)
+                {
+                    replyMessageStringBuilder.Append($"Hello, I am **ZaraCodeWeek** Bot");
+                    replyMessageStringBuilder.Append($"\n");
+                    replyMessageStringBuilder.Append($"You can say anything");
+                    replyMessageStringBuilder.Append($"\n");
+                    replyMessageStringBuilder.Append($"to me and I will repeat it back");
+                    replyMessageStringBuilder.Append($"\n\n");
+                    replyMessageStringBuilder.Append($"What is your name?");
+
+                    userData.SetProperty<bool>(KeyAskedForUsernameString, true);
+                }
+                else
+                {
+                    if (string.IsNullOrWhiteSpace(username))
+                    {
+                        replyMessageStringBuilder.Append($"Hello {activity.Text}");
+
+                        userData.SetProperty<string>(KeyUsernameString, activity.Text);
+                    }
+                    else
+                    {
+                        replyMessageStringBuilder.Append($"{username}, you said: {activity.Text}");
+                    }
+                }
+
+                // Save BotUserData
+                stateClient.BotState.SetPrivateConversationData(
+                    activity.ChannelId,
+                    activity.Conversation.Id,
+                    activity.From.Id,
+                    userData);
+
+                // Create reply message
                 var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                var replyMessage = activity.CreateReply(replyMessageStringBuilder.ToString());
 
-                // calculate something for us to return
-                var length = (activity.Text ?? string.Empty).Length;
-
-                // return our reply to the user
-                var reply = activity.CreateReply($"Hello there! You sent {length} characters.");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                await connector.Conversations.ReplyToActivityAsync(replyMessage);
             }
             else
             {
-                this.HandleSystemMessage(activity);
+                var replyMessage = HandleSystemMessage(activity);
+                if (replyMessage != null)
+                {
+                    var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                    await connector.Conversations.ReplyToActivityAsync(replyMessage);
+                }
             }
 
+            // Return response
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
